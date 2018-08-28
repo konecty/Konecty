ed1d355e8028b9c2dfcc7c1341247dc897854c2c
Derotino Silveira <silveirado@gmail.com>
2018-08-28 16:12:06 -0300

    DS208 suggestions

--- packages/konsistent/lib/htmlParser.js
+++ packages/konsistent/lib/htmlParser.js
@@ -1,12 +1,5 @@
-/*
- * decaffeinate suggestions:
- * DS208: Avoid top-level this
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-({
-  [this.htmlParser](doc) {
-    console.log('Documento a ser parseado para HTML ->', doc);
-    const parser = new DOMParser();
-    return parser.parseFromString(doc, 'text/html');
-  }
-});
+htmlParser = doc => {
+  console.log('Documento a ser parseado para HTML ->', doc);
+  const parser = new DOMParser();
+  return parser.parseFromString(doc, 'text/html');
+};
--- server/lib/model/Models.js
+++ server/lib/model/Models.js
@@ -1,6 +1,1 @@
-/*
- * decaffeinate suggestions:
- * DS208: Avoid top-level this
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-this.Models = {};
+Models = {};
