c5d22a4c71d65eb0103674e9ce74a8d784b01e18
Derotino Silveira <silveirado@gmail.com>
2023-06-07 18:56:38 -0300

    refactor: remove obsolete zapier integration

--- server/routes/rest/process/processApi.js
+++ server/routes/rest/process/processApi.js
@@ -4,19 +4,10 @@ import { app } from '/server/lib/routes/app';
 import { sessionUtils } from '/imports/utils/sessionUtils';
 
 app.post('/rest/process/submit', (req, res, next) =>
-  res.send(
-    Meteor.call('process:submit', {
-      authTokenId: sessionUtils.getAuthTokenIdFromReq(req),
-      data: req.body.data
-    })
-  )
-);
-
-app.post('/rest/process/zapier', (req, res, next) =>
-  res.send(
-    Meteor.call('process:zapier', {
-      authTokenId: sessionUtils.getAuthTokenIdFromReq(req),
-      data: req.body
-    })
-  )
+	res.send(
+		Meteor.call('process:submit', {
+			authTokenId: sessionUtils.getAuthTokenIdFromReq(req),
+			data: req.body.data,
+		}),
+	),
 );
