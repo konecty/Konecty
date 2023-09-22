87ccf8f6cb6e086f71bf87c5e551878e0828d829
Derotino Silveira <silveirado@gmail.com>
2023-09-22 17:27:42 -0300

    refactor: demeteorize health endpoints

--- server/routes/rest/health/index.js
+++ server/routes/rest/health/index.js
@@ -1,34 +1,18 @@
-import { Meteor } from 'meteor/meteor';
-
-import { MetaObject } from '/imports/model/MetaObject';
+import { MetaObjectCollection } from '/imports/model/MetaObject';
 
 import { app } from '/server/lib/routes/app';
 import { logger } from '/imports/utils/logger';
 
-let readiness = false;
-
 app.get('/readiness', (_, res) => {
-	if (readiness) {
-		res.send(200, 'OK');
-	} else {
-		res.send(503, 'Not ready');
-	}
+	res.send(200, 'OK');
 });
 
 app.get('/liveness', async (_, res) => {
-	if (readiness) {
-		try {
-			await MetaObject.findOne({ _id: 'Namespace' });
-			res.send('OK');
-		} catch (error) {
-			logger.error(error, `Error on liveness (${new Date().toISOString()}): ${error.message}`);
-			res.send(503, 'The king is dead, long live the king!');
-		}
-	} else {
-		res.send(503, 'Not ready');
+	try {
+		await MetaObjectCollection.findOne({ _id: 'Namespace' });
+		res.send('OK');
+	} catch (error) {
+		logger.error(error, `Error on liveness (${new Date().toISOString()}): ${error.message}`);
+		res.send(503, 'The king is dead, long live the king!');
 	}
-});
-
-Meteor.startup(() => {
-	readiness = true;
 });
