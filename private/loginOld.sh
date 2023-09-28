579657b3d0a2df9a70fc5c24f69e0774e40406c4
Derotino Silveira <silveirado@gmail.com>
2023-09-28 10:52:45 -0300

    refactor: remove deadcode

--- private/loginOld.sh
+++
@@ -1,4 +0,0 @@
-
-curl -i --data "user=admin&password=eab5a46056bf1936911e588c607786f8&ns=egalite&geolocation=%7B%22lat%22%3A-30.030703499999998%2C%22lng%22%3A-51.1818877%7D&resolution=%7B%22height%22%3A800%2C%22width%22%3A1280%7D" "http://konecty.local/rest/auth/login"
-
-curl -i --data "user=admin&password=eab5a46056bf1936911e588c607786f8&ns=egalite&geolocation=%7B%22lat%22%3A-30.030703499999998%2C%22lng%22%3A-51.1818877%7D&resolution=%7B%22height%22%3A800%2C%22width%22%3A1280%7D" "http://localhost:3000/rest/auth/login"
