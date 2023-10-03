3e9e355b553c5b3dccf17779d83c77ed63a2cf50
Derotino Silveira <silveirado@gmail.com>
2023-10-03 19:41:36 -0300

    refactor: demeteorize konsistent

--- server/startup/konsistent.js
+++ server/startup/konsistent.js
@@ -1,9 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
-import { Konsistent } from '/imports/konsistent';
+import { setupKonsistent } from '/imports/konsistent';
 import { logger } from '/imports/utils/logger';
 import { start as startSendMail } from '/imports/mailConsumer';
+import { db } from '/imports/database';
 
 Accounts.config({ forbidClientAccountCreation: true });
 
@@ -18,7 +19,10 @@ Meteor.startup(function () {
 	if (/true|1|enabled/i.test(process.env.DISABLE_KONSISTENT)) {
 		logger.warn('[konsistent] Konsistent is disabled');
 	} else {
-		logger.info('[konsistent] Starting up');
-		Konsistent.start();
+		logger.info(`[konsistent]  === ${db.databaseName} === Starting up`);
+
+		setupKonsistent().catch(err => {
+			logger.error(err, `[konsistent]  === ${db.databaseName} === Error while setting up`);
+		});
 	}
 });
