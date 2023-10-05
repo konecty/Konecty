a468398e2945ead20f03d1037fb2fdad7f27b023
Matheus Jacks <matheus.jacks@konecty.com>
2023-10-04 23:54:54 -0300

    feat: update listview api to return filters

--- imports/list-view/index.ts
+++ imports/list-view/index.ts
@@ -1,9 +1,8 @@
 import sortBy from 'lodash/sortBy';
-
-import { MetaObjectCollection } from '/imports/model/MetaObject';
 import { DocumentListSchema } from '/imports/model/DocumentList';
-import { logger } from '/imports/utils/logger';
+import { MetaObjectCollection } from '/imports/model/MetaObject';
 import { MetaObjectType } from '/imports/types/metadata';
+import { logger } from '/imports/utils/logger';
 
 export async function listView(document: string, id: string) {
 	const list = await MetaObjectCollection.findOne<MetaObjectType>({ type: 'list', document: document, name: id, menuSorter: { $nin: [-1, -2, -3] } });
@@ -16,8 +15,14 @@ export async function listView(document: string, id: string) {
 
 	const listColumns = sortBy(columnsArray, ['sort', 'name']);
 
+	const filtersList = sortBy(
+		Object.entries(list.filter?.conditions).map(([, value]) => value),
+		['sort', 'term'],
+	);
+
 	const listViewResult = DocumentListSchema.safeParse({
 		...list,
+		filter: filtersList,
 		document,
 		columns: listColumns,
 		label: list.label['en'],
