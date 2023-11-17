5d55db8df03ca01ec159db12036c1a916eb8ffdb
Derotino Silveira <silveirado@gmail.com>
2023-11-17 09:14:06 -0300

    fix: fix mongourl parser for atlas

--- src/imports/database/index.ts
+++ src/imports/database/index.ts
@@ -3,19 +3,25 @@ import querystring from 'node:querystring';
 
 import { logger } from '../utils/logger';
 
-const MONGODB_URL_REGEX = /^mongodb:\/\/(?:([^:]+):([^@]+)@)?([^/:]+)(?::(\d+))?\/([^?]+)(?:\?(.+))?$/;
+const MONGODB_URL_REGEX = /^(mongodb|mongodb\+srv):\/\/(?:([^:]+):([^@]+)@)?([^/:]+)(?::(\d+))?\/([^?]+)(?:\?(.+))?$/;
 
 const MONGO_URL = process.env.MONGO_URL ?? 'mongodb://localhost:27017';
 
-const [, username, password, host, port, database, urioptions] = MONGO_URL.match(MONGODB_URL_REGEX) || [];
+const [, protocol, username, password, host, port, database, urioptions] = MONGO_URL.match(MONGODB_URL_REGEX) || [];
 
 const options = querystring.parse(urioptions ?? {});
 
 const getMongoUrl = () => {
 	if (username != null && password != null) {
-		return `mongodb://${username}:${password}@${host}:${port ?? 27017}`;
+		if (protocol === 'mongodb+srv') {
+			return `${protocol}://${username}:${password}@${host}/${database}`;
+		}
+		return `${protocol}://${username}:${password}@${host}:${port ?? 27017}`;
 	}
-	return `mongodb://${host}:${port ?? 27017}`;
+	if (protocol === 'mongodb+srv') {
+		return `${protocol}://${host}/${database}`;
+	}
+	return `${protocol}://${host}:${port ?? 27017}`;
 };
 
 const mongoUrl = getMongoUrl();
