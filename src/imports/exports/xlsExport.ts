a99ffa821f383afd0e921b1ae9651b051614e161
Leonardo Viva <leonardo.leal@konecty.com>
2024-05-02 15:51:39 -0300

    fix: xls export

--- src/imports/exports/xlsExport.ts
+++ src/imports/exports/xlsExport.ts
@@ -10,54 +10,28 @@ import { Readable } from 'stream';
 export default async function xlsExport(dataStream: Readable, name: string): Promise<KonectyResult<ExportDataResponse>> {
 	const wb = new Workbook();
 
-	const headerStyle = wb.createStyle({
-		font: {
-			bold: true,
-		},
-		fill: {
-			type: 'pattern',
-			patternType: 'solid',
-			fgColor: '#F2F2F2',
-		},
-	});
-
 	const ws = wb.addWorksheet(name);
 	const flattenData = new TransformFlattenData('dd/MM/yyyy HH:mm:ss');
 
 	const widths: Record<string, number> = {};
 
-	dataStream.pipe(flattenData).reduce(addToWorksheet(flattenData, widths), ws);
-
-	const addHeaders = () => {
-		const headers = Array.from(flattenData.headers);
-		const headerNum = headers.length;
-
-		for (let collumn = 1; collumn <= headerNum; collumn++) {
-			const header = headers[collumn - 1];
-			const cell = ws.cell(1, collumn);
-
-			cell.string(header).style(headerStyle);
-			ws.column(collumn).setWidth(widths[header] ?? 10);
-		}
-	};
-
-	return new Promise((resolve, reject) => {
-		dataStream.on('end', async () => {
-			addHeaders();
-			resolve(
-				successReturn({
-					httpHeaders: {
-						'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-						'Content-Disposition': `attachment; filename=${name}.xlsx`,
-					},
-					content: await wb.writeToBuffer(),
-				}),
-			);
-		});
-
+	return new Promise(async (resolve, reject) => {
 		dataStream.on('error', err => {
 			reject(errorReturn('Error writing file'));
 		});
+
+		await dataStream.pipe(flattenData).reduce(addToWorksheet(flattenData, widths), ws);
+		addHeaders(wb, ws, Array.from(flattenData.headers), widths);
+
+		resolve(
+			successReturn({
+				httpHeaders: {
+					'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+					'Content-Disposition': `attachment; filename=${name}.xlsx`,
+				},
+				content: await wb.writeToBuffer(),
+			}),
+		);
 	});
 }
 
@@ -89,3 +63,26 @@ function addToWorksheet(flattenData: TransformFlattenData, widths: Record<string
 		return acc;
 	};
 }
+
+const addHeaders = (wb: Workbook, ws: Worksheet, headers: string[], widths: Record<string, number>) => {
+	const headerStyle = wb.createStyle({
+		font: {
+			bold: true,
+		},
+		fill: {
+			type: 'pattern',
+			patternType: 'solid',
+			fgColor: '#F2F2F2',
+		},
+	});
+
+	const headerNum = headers.length;
+
+	for (let collumn = 1; collumn <= headerNum; collumn++) {
+		const header = headers[collumn - 1];
+		const cell = ws.cell(1, collumn);
+
+		cell.string(header).style(headerStyle);
+		ws.column(collumn).setWidth(widths[header] ?? 10);
+	}
+};
