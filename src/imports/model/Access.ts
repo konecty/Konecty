0cea37b3909f524bc60cd9e9912b378db5866dcb
Marcus de Moura <marcusdemoura@gmail.com>
2024-06-19 11:53:33 +0200

    fix: passthrough access props

--- src/imports/model/Access.ts
+++ src/imports/model/Access.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod';
 
-export const AccessSchema = z.object({
-	type: z.literal('access'),
-	_id: z.string(),
-	document: z.string(),
-	name: z.string(),
-	collection: z.string().optional(),
-});
+export const AccessSchema = z
+	.object({
+		type: z.literal('access'),
+		_id: z.string(),
+		document: z.string(),
+		name: z.string(),
+		collection: z.string().optional(),
+	})
+	.passthrough();
 
 export type Access = z.infer<typeof AccessSchema>;
