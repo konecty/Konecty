8c33b2cd98e2bbc97b2c0674bf1f5e74785e56ea
Leonardo Viva <leonardo.leal@konecty.com>
2025-01-07 17:32:35 -0300

    fix: send message retries

--- src/imports/queue/RabbitMQResource.ts
+++ src/imports/queue/RabbitMQResource.ts
@@ -1,3 +1,4 @@
+import { KonectyResult } from '@imports/types/result';
 import { errorReturn, successReturn } from '@imports/utils/return';
 import { Channel, connect, Connection } from 'amqplib';
 import { QueueResource } from './QueueResource';
@@ -5,9 +6,11 @@ import { QueueResource } from './QueueResource';
 export class RabbitMQResource extends QueueResource {
 	private connection: Connection | null = null;
 	private channel: Channel | null = null;
+	private connectionUrl: string = '';
 
 	async connect(url: string): Promise<void> {
 		try {
+			this.connectionUrl = url;
 			this.connection = await connect(url);
 			this.channel = await this.connection.createChannel();
 
@@ -38,10 +41,21 @@ export class RabbitMQResource extends QueueResource {
 		await this.channel?.assertQueue(name, driverParams);
 	}
 
-	async sendMessage(queue: string, message: unknown) {
-		const strMessage = typeof message === 'object' ? JSON.stringify(message) : String(message);
-		const success = this.channel?.sendToQueue(queue, Buffer.from(strMessage), { appId: 'konecty' });
+	async sendMessage(queue: string, message: unknown, retries: number = 0): Promise<KonectyResult> {
+		try {
+			const strMessage = typeof message === 'object' ? JSON.stringify(message) : String(message);
+			const success = this.channel?.sendToQueue(queue, Buffer.from(strMessage), { appId: 'konecty', persistent: true, deliveryMode: 2 });
 
-		return success ? successReturn('Message sent') : errorReturn('Failed to send message');
+			return success ? successReturn('Message sent') : errorReturn('Failed to send message');
+		} catch (error) {
+			if (retries > 2) {
+				await this.handleError(error as Error, 'sendMessage');
+				return errorReturn('Failed to send message');
+			}
+
+			await this.disconnect();
+			await this.handleConnectionError(error as Error, () => this.connect(this.connectionUrl));
+			return this.sendMessage(queue, message, retries + 1);
+		}
 	}
 }
