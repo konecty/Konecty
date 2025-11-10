623d7f7fb26b982b7992e2ffe6ee9f054a292385
Marcus de Moura <marcusdemoura@gmail.com>
2025-11-10 17:37:24 +0100

    feat: dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'src/private/templates/login/modern.hbs',
-		'src/private/templates/login/modern-tailwind-input.css',
-	],
+	darkMode: 'media',
+	content: ['src/private/templates/login/modern.hbs', 'src/private/templates/login/modern-tailwind-input.css'],
 	theme: {
 		extend: {},
 	},
